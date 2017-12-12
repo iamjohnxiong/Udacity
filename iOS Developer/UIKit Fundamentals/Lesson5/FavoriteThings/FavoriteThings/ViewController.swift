1ca253556a27a16b9ab240993a3c976cc2d75f72
John Xiong <iamjohnxiong@gmail.com>
2017-12-11 19:52:23 -0500

    Init for Lesson 5
    Add Bond Villains and Favorite Things.

---
+++ iOS Developer/UIKit Fundamentals/Lesson5/FavoriteThings/FavoriteThings/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  FavoriteThings
+//
+//  Created by Jason Schatz on 11/18/14.
+//  Copyright (c) 2014 Udacity. All rights reserved.
+//
+
+import UIKit
+
+// MARK: - ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    // MARK: Model
+    
+    let favoriteThings: [String] = [
+        "Hello",
+        "World"
+    ]
+
+    // MARK: Table View Data Source Methods
+    
+    // number of rows
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let placeholderCount = favoriteThings.count
+        return placeholderCount
+    }
+    
+    // cell for row at index path
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "FavoriteThingCell")!
+        cell.textLabel?.text = self.favoriteThings[indexPath.row]
+        return cell
+    }
+}
