431f0f8468ec5cb5aea586e1341001b7d75937b8
John Xiong <iamjohnxiong@gmail.com>
2018-01-24 17:51:29 -0500

    Complete SITL

--- iOS Developer/iOS Networking with Swift/Lesson2/SleepingInTheLibrary/ViewController.swift
+++ iOS Developer/iOS Networking with Swift/Lesson2/SleepingInTheLibrary/ViewController.swift
@@ -51,20 +51,53 @@ class ViewController: UIViewController {
             Constants.FlickrParameterKeys.NoJSONCallback: Constants.FlickrParameterValues.DisableJSONCallback
         ]
         
-        let urlString = escapedParameters(parameters: methodParameters as [String: AnyObject])
+        let urlString = Constants.Flickr.APIBaseURL + escapedParameters(parameters: methodParameters as [String: AnyObject])
         let url = URL(string: urlString)!
         let urlRequest = URLRequest(url: url)
         
         let task = URLSession.shared.dataTask(with: urlRequest) { (data, urlResponse, error) in
+            func displayError(_ error: String) {
+                print(error)
+                print("URL at time of error: \(url)")
+                performUIUpdatesOnMain {
+                    self.setUIEnabled(true)
+                }
+            }
+            
             if error == nil {
-                print("completed")
-                print(data!)
+                if let data = data {
+                    let parsedResult: [String:AnyObject]!
+                    do {
+                        parsedResult = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as! [String:AnyObject]
+                    } catch {
+                        displayError("Could not parse the data as JSON: '\(data)'")
+                        return
+                    }
+                    
+                    if let photosDictionary = parsedResult[Constants.FlickrResponseKeys.Photos] as? [String: AnyObject],
+                        let photoArray = photosDictionary[Constants.FlickrResponseKeys.Photo] as? [[String: AnyObject]] {
+                        let randomIndex = Int(arc4random_uniform(UInt32(photoArray.count)))
+                        let photoDictionary = photoArray[randomIndex] as [String: AnyObject]
+                        
+                        if let imageUrlString = photoDictionary[Constants.FlickrResponseKeys.MediumURL] as? String,
+                            let photoTitle = photoDictionary[Constants.FlickrResponseKeys.Title] as? String {
+                            let imageUrl = URL(string: imageUrlString)
+                            if let imageData = try? Data(contentsOf: imageUrl!) {
+                                performUIUpdatesOnMain {
+                                    self.photoImageView.image = UIImage(data: imageData)
+                                    self.photoTitleLabel.text = photoTitle
+                                }
+                            }
+                        }
+                        
+                    }
+                }
+                
             } else {
                 print("error: " + error.debugDescription)
             }
         }
         
-        print("resume")
         task.resume()
     }
     
