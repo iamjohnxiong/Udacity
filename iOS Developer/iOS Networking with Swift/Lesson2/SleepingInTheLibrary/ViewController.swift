fcaaff4456c1cd9028a1799ad9800a25431ada56
John Xiong <iamjohnxiong@gmail.com>
2018-01-23 20:00:58 -0500

    Init commit for sleep in lib app

---
+++ iOS Developer/iOS Networking with Swift/Lesson2/SleepingInTheLibrary/ViewController.swift
@@ -0,0 +1,88 @@
+//
+//  ViewController.swift
+//  SleepingInTheLibrary
+//
+//  Created by Jarrod Parkes on 11/3/15.
+//  Copyright © 2015 Udacity. All rights reserved.
+//
+
+import UIKit
+
+// MARK: - ViewController: UIViewController
+
+class ViewController: UIViewController {
+
+    // MARK: Outlets
+    
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var photoTitleLabel: UILabel!
+    @IBOutlet weak var grabImageButton: UIButton!
+    
+    // MARK: Actions
+    
+    @IBAction func grabNewImage(_ sender: AnyObject) {
+        setUIEnabled(false)
+        getImageFromFlickr()
+    }
+    
+    // MARK: Configure UI
+    
+    private func setUIEnabled(_ enabled: Bool) {
+        photoTitleLabel.isEnabled = enabled
+        grabImageButton.isEnabled = enabled
+        
+        if enabled {
+            grabImageButton.alpha = 1.0
+        } else {
+            grabImageButton.alpha = 0.5
+        }
+    }
+    
+    // MARK: Make Network Request
+    
+    private func getImageFromFlickr() {
+        
+        let methodParameters = [
+            Constants.FlickrParameterKeys.Method: Constants.FlickrParameterValues.GalleryPhotosMethod,
+            Constants.FlickrParameterKeys.APIKey: Constants.FlickrParameterValues.APIKey,
+            Constants.FlickrParameterKeys.GalleryID: Constants.FlickrParameterValues.GalleryID,
+            Constants.FlickrParameterKeys.Extras: Constants.FlickrParameterValues.MediumURL,
+            Constants.FlickrParameterKeys.Format: Constants.FlickrParameterValues.ResponseFormat,
+            Constants.FlickrParameterKeys.NoJSONCallback: Constants.FlickrParameterValues.DisableJSONCallback
+        ]
+        
+        let urlString = escapedParameters(parameters: methodParameters as [String: AnyObject])
+        let url = URL(string: urlString)!
+        let urlRequest = URLRequest(url: url)
+        
+        let task = URLSession.shared.dataTask(with: urlRequest) { (data, urlResponse, error) in
+            if error == nil {
+                print("completed")
+                print(data!)
+            } else {
+                print("error: " + error.debugDescription)
+            }
+        }
+        
+        print("resume")
+        task.resume()
+    }
+    
+    private func escapedParameters(parameters: [String: AnyObject]) -> String {
+        
+        if !parameters.isEmpty {
+            
+            var keyValuePairs = [String]()
+            
+            for (key, value) in parameters {
+                let stringValue = "\(value)"
+                let escapedValue = stringValue.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
+                keyValuePairs.append(key + "=" + "\(escapedValue!)")
+            }
+            
+            return "?\(keyValuePairs.joined(separator: "&"))"
+        } else {
+            return ""
+        }
+    }
+}
